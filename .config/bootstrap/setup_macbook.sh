3e1aaf7412e5e56ac0dd542812c8aaf1ff2f24c5
Michael Grunewalder <michael.grunewalder@MacBook-Pro-von-Michael.fritz.box>
2025-05-24 22:18:21 +0200

    refactored setup script

--- .config/bootstrap/setup_macbook.sh
+++ .config/bootstrap/setup_macbook.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#######################Install XCode CMD Tools
+echo "Installing commandline tools..."
+xcode-select --install
+
 # Check if Homebrew is already installed
 if ! command -v brew &> /dev/null
 then
@@ -11,69 +15,75 @@ else
     echo "Homebrew is already installed."
 fi
 
+###################Installing YADM to get the configurations
+brew install yadm
+
+
+echo "Getting configuration from YADM"
+#rm -rf ~/.zshrc
+#yadm clone git@github.com:michael-grunewalder/dotfile.git
+yadm clone https://github.com/michael-grunewalder/dotfile.git
+yadm status
+yadm checkout -f
+
+##################PPLY THE CONDFIGURATION
+source ~/.config/zsh/env.zsh
+
+
 # Update Homebrew
 echo "Updating Homebrew..."
 brew update
 
-# List of packages to install
-packages=(
-"--cask ghostty"
-    "--cask font-hack-nerd-font"
-    "--cask sf-symbols"
-    "--cask font-meslo-lg-nerd-font"
-    "--cask sublime-text"
-    "--cask font-sf-mono"
-    "--cask visual-studio-code"
-    "--cask font-sf-pro"
-    "--cask warp"
-    "--cask font-sketchybar-app-font"
-    "--cask appcleaner"
-    "--cask herd"
-    "--cask opera-air"
-    "--cask arc"
-    "--cask google-chrome"
-    "--cask firefox"
-    "--cask dbngin"
-    "--cask textmate"
-    "--cask setapp"
-    "screenresolution"
-    "eza"
-    "sketchybar"
-    "fzf"
-    "mas"
-    "starship"
-    "midnight-commander"
-    "switchaudio-osx"
-    "tmux"
-    "jesseduffield/lazygit/lazygit"
-    "neofetch"
-    "nowplaying-cli"
-    "yadm"
-	"ffmpeg"
-	"sevenzip"
-	"jq"
-	"poppler"
-	"fd"
-	"ripgrep"
-	"fzf"
-	"zoxide"
-	"resvg"
-	"imagemagick"
-	"yazi"
-	"font-symbols-only-nerd-font"
-    "--no-quarantine grishka/grishka/neardrop"
-)
+################Installing apps from  Homebrew
+brew install screenresolution
+brew install eza
+brew install sketchybar
+brew install fzf
+brew install mas
+brew install starship
+brew install midnight-commander
+brew install switchaudio-osx
+brew install tmux
+brew install jesseduffield/lazygit/lazygit
+brew install neofetch
+brew install nowplaying-cli
+brew install ffmpeg
+brew install sevenzip
+brew install jq
+brew install poppler
+brew install fd
+brew install ripgrep
+brew install fzf
+brew install zoxide
+brew install resvg
+brew install imagemagick
+brew install yazi
+brew install font-symbols-only-nerd-font
 
-# Install packages
-echo "Installing packages..."
-brew tap FelixKratz/formulae
-for package in "${packages[@]}"
-do
-    echo "Installing $package..."
-    brew install $package
-done
-echo "Upgrading packages..."
-brew upgrade
+#####uncomment this f you want to use Android phones again
+# brew install --no-quarantine grishka/grishka/neardrop
+
+################Installing CASKS
+brew install --cask --no-quarantine ghostty
+brew install --cask --no-quarantine font-hack-nerd-font
+brew install --cask --no-quarantine sf-symbols
+brew install --cask --no-quarantine font-meslo-lg-nerd-font
+brew install --cask --no-quarantine sublime-text
+brew install --cask --no-quarantine font-sf-mono
+brew install --cask --no-quarantine visual-studio-code
+brew install --cask --no-quarantine font-sf-pro
+brew install --cask --no-quarantine warp
+brew install --cask --no-quarantine font-sketchybar-app-font
+brew install --cask --no-quarantine appcleaner
+brew install --cask --no-quarantine herd
+brew install --cask --no-quarantine opera-air
+brew install --cask --no-quarantine arc
+brew install --cask --no-quarantine google-chrome
+brew install --cask --no-quarantine firefox
+brew install --cask --no-quarantine dbngin
+brew install --cask --no-quarantine textmate
+brew install --cask --no-quarantine setapp
+
 echo "Installation of Homebrew Apps complete!"
 echo "***************************************************DONE************************************"
 
@@ -90,12 +100,21 @@ mas install 1354318707
 echo "Important Apps installed"
 echo "***************************************************DONE************************************"
 
-echo "Getting configuration from YADM"
-#rm -rf ~/.zshrc
-#yadm clone git@github.com:michael-grunewalder/dotfile.git
-yadm clone https://github.com/michael-grunewalder/dotfile.git
-yadm status
-yadm checkout -f
-
-defaults write 'Apple Global Domain' AppleMenuBarVisibleInFullscreen -bool false && defaults write 'Apple Global Domain' _HIHideMenuBar -bool true
+echo "Changing macOS defaults..."
+defaults write NSGlobalDomain _HIHideMenuBar -bool true
+defaults write NSGlobalDomain AppleHighlightColor -string "0.65098 0.85490 0.58431"
+defaults write NSGlobalDomain AppleAccentColor -int 1
+defaults write com.apple.screencapture location -string "$HOME/Desktop"
+defaults write com.apple.screencapture disable-shadow -bool false
+defaults write com.apple.screencapture type -string "png"
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+defaults write com.apple.finder ShowStatusBar -bool false
+defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
+defaults write NSGlobalDomain WebKitDeveloperExtras -bool truescreen -bool false && defaults write 'Apple Global Domain' _HIHideMenuBar -bool true
 brew services start sketchybar
