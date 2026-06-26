28357e3859d223b91413cc99abad446278f06b9e
Michael Grunewalder <michael.grunewalder@MacBook-Pro-von-Michael.local>
2026-06-26 23:25:35 +0200

    2026-06-26

--- .config/bootstrap/setup_macbook.sh
+++ .config/bootstrap/setup_macbook.sh
@@ -172,11 +172,9 @@ echo "Installing Apple AppStore Applications"
 MAS_APPS=(
     "1099475282:SnailGit Lite"
     "1436522307:Transmit 5"
-    "1352778147:Bitwarden"
     "1354318707:Core Tunnel"
     "1631624924:Final Cut Pro"
     "6743048714:Googly Eyes"
-    "1452453066:Hidden Bar"
     "1615087040:Logic Pro"
     "6746637089:MainStage"
     "6754022187:Menu Drop"
@@ -202,7 +200,7 @@ echo "Changing macOS defaults..."
 mkdir "$HOME/Desktop/Screenshots"
 # defaults write NSGlobalDomain _HIHideMenuBar -bool true
 # defaults write NSGlobalDomain AppleHighlightColor -string "0.47 0.65 0.9"
-defaults write NSGlobalDomain AppleAccentColor -int 1
+# defaults write NSGlobalDomain AppleAccentColor -int 1
 defaults write com.apple.screencapture location -string "$HOME/Desktop/Screenshots"
 defaults write com.apple.screencapture disable-shadow -bool false
 defaults write com.apple.screencapture type -string "png"
